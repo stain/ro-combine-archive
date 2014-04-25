a3b01cd1a2a2b2b9d75ded419acb2514978d819a
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-25 10:19:29 +0100

    First attempt to enrich Combine Archive as RO Bundle

---
+++ src/main/java/org/researchobject/combinearchive/CombineToRO.java
@@ -0,0 +1,82 @@
+package org.researchobject.combinearchive;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.purl.wf4ever.robundle.Bundle;
+import org.purl.wf4ever.robundle.Bundles;
+import org.purl.wf4ever.robundle.manifest.Manifest;
+import org.purl.wf4ever.robundle.manifest.PathMetadata;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class CombineToRO {
+
+	public static void main(String[] args) throws Exception {
+		for (String arg : args) {
+			Path file = Paths.get(arg);
+			combineToRO(file);
+		}
+	}
+
+	public static void combineToRO(Path file) throws IOException, SAXException, ParserConfigurationException {		
+		try (Bundle bundle = Bundles.openBundle(file)) {
+			Path manifestXml = bundle.getFileSystem().getPath("/manifest.xml");
+			if (Files.exists(manifestXml)) {
+				Manifest roManifest = bundle.getManifest();
+				roManifest.getManifest().add(manifestXml);
+				roManifest.setCreatedOn(Files.getLastModifiedTime(manifestXml));
+				
+				
+				// FIXME: Need a single method to avoid this
+				List<PathMetadata> aggregates = roManifest.getAggregates();
+				aggregates.remove(roManifest.getAggregation(manifestXml));
+				roManifest.setAggregates(aggregates);
+				
+				
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				factory.setNamespaceAware(true);
+				DocumentBuilder db = factory.newDocumentBuilder();
+				try (InputStream input = Files.newInputStream(manifestXml)) {				
+					Document a = db.parse(input);
+					NodeList contentNodes = a.getElementsByTagNameNS("http://identifiers.org/combine.specifications/omex-manifest", "content");
+					for (int i=0; i<contentNodes.getLength(); i++) {
+						Node item = contentNodes.item(i);
+						String location = item.getAttributes().getNamedItem("location").getTextContent();
+						String format = item.getAttributes().getNamedItem("format").getTextContent();
+						URI loc = manifestXml.toUri().resolve(location);
+						//System.out.println(loc);
+						if (! bundle.getRoot().toUri().relativize(loc).isAbsolute() &&
+								! Files.exists(bundle.getFileSystem().provider().getPath(loc))) { 
+							System.out.println("Missing: " + location);
+							continue;
+						}
+					
+						
+						PathMetadata aggr = roManifest.getAggregation(loc);
+						aggr.setMediatype(format);
+					}
+				}
+			}			
+			
+//			List<Path> items = null; // TODO: Get from manifest 
+//			for (Path p : items) {
+//				PathMetadata aggr = bundle.getManifest().getAggregation(p);
+//				
+		}
+	}
+
+}
+ 
---
+++ src/test/java/org/researchobject/combinearchive/TestCombineToRO.java
@@ -0,0 +1,41 @@
+package org.researchobject.combinearchive;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import org.junit.Test;
+
+public class TestCombineToRO {
+
+	@Test
+	public void convertBoris() throws Exception {
+		Path file = Files.createTempFile("Boris", ".omex");
+		try (InputStream src = getClass().getResourceAsStream("/Boris.omex")) {
+			Files.copy(src, file, StandardCopyOption.REPLACE_EXISTING);
+		}
+		CombineToRO.combineToRO(file);
+		System.out.println(file);		
+	}
+	
+	@Test
+	public void convertDirectoryMadness() throws Exception {
+		Path file = Files.createTempFile("DirectoryMadness", ".omex");
+		try (InputStream src = getClass().getResourceAsStream("/DirectoryMadness.omex")) {
+			Files.copy(src, file, StandardCopyOption.REPLACE_EXISTING);
+		}
+		CombineToRO.combineToRO(file);
+		System.out.println(file);		
+	}
+	
+	@Test
+	public void convertDirectoryMadnessZipped() throws Exception {
+		Path file = Files.createTempFile("DirectoryMadnessZipped", ".omex");
+		try (InputStream src = getClass().getResourceAsStream("/DirectoryMadnessZipped.omex")) {
+			Files.copy(src, file, StandardCopyOption.REPLACE_EXISTING);
+		}
+		CombineToRO.combineToRO(file);
+		System.out.println(file);		
+	}
+}
