d5b593f6f07352b9467c259f1563a08a05fb48b7
stain <stain@biggie-mint>
2014-04-28 16:00:31 +0100

    test creator/created/mediatype of aslanidi file

--- src/test/java/org/researchobject/combinearchive/TestCombineToRO.java
+++ src/test/java/org/researchobject/combinearchive/TestCombineToRO.java
@@ -1,5 +1,7 @@
 package org.researchobject.combinearchive;
 
+import static org.junit.Assert.*;
+
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +12,11 @@ import java.util.logging.Logger;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.purl.wf4ever.robundle.Bundle;
+import org.purl.wf4ever.robundle.Bundles;
+import org.purl.wf4ever.robundle.manifest.Agent;
+import org.purl.wf4ever.robundle.manifest.Manifest;
+import org.purl.wf4ever.robundle.manifest.PathMetadata;
 
 public class TestCombineToRO {
 
@@ -31,7 +38,27 @@ public class TestCombineToRO {
 			Files.copy(src, file, StandardCopyOption.REPLACE_EXISTING);
 		}
 		CombineToRO.combineToRO(file);
-		System.out.println(file);		
+		System.out.println(file);
+		try (Bundle bundle = Bundles.openBundle(file)) {
+			Manifest manifest = manifest = bundle.getManifest();
+			Path manifestXml = bundle.getRoot().resolve("manifest.xml");
+			//assertTrue("manifest.xml not listed in " + manifest.getManifest(), 
+			//		manifest.getManifest().contains(manifestXml));
+			
+//			Agent createdBy = manifest.getCreatedBy().get(0);
+//			assertEquals("Gary Mirams", createdBy.getName());
+//			assertEquals("mbox:gary.mirams@cs.ox.ac.uk", createdBy.getUri());
+			
+			Path csvPath = bundle.getRoot().resolve("outputs_degree_of_block.csv");			
+			PathMetadata csv = manifest.getAggregation(csvPath);
+			assertEquals("text/csv", csv.getMediatype());
+			Agent csvCreator = csv.getCreatedBy().get(0);
+			assertEquals("Gary Mirams", csvCreator.getName());
+			assertEquals("mbox:gary.mirams@cs.ox.ac.uk", csvCreator.getUri().toString());
+			assertEquals("2014-02-06T22:01:58Z", csv.getCreatedOn().toString());
+			
+			
+		}
 	}
 	
 	@Test
