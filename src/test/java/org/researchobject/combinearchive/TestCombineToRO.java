dae252293e3a8deb0e8a1a9fdc458925bdac7e84
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-28 17:05:12 +0100

    test media type and conforms to, including external URI

--- src/test/java/org/researchobject/combinearchive/TestCombineToRO.java
+++ src/test/java/org/researchobject/combinearchive/TestCombineToRO.java
@@ -1,12 +1,13 @@
 package org.researchobject.combinearchive;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.List;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -72,21 +73,22 @@ public class TestCombineToRO {
 		}
 		CombineToRO.combineToRO(file);
 		System.out.println(file);		
-//		try (Bundle bundle = Bundles.openBundle(file)) {
-//			Manifest manifest = manifest = bundle.getManifest();
-//			Path manifestXml = bundle.getRoot().resolve("manifest.xml");
-//			assertTrue("manifest.xml not listed in " + manifest.getManifest(), 
-//					manifest.getManifest().contains(manifestXml));
-//			
+		try (Bundle bundle = Bundles.openBundle(file)) {
+			Manifest manifest = manifest = bundle.getManifest();
+			Path manifestXml = bundle.getRoot().resolve("manifest.xml");
+			assertTrue("manifest.xml not listed in " + manifest.getManifest(), 
+					manifest.getManifest().contains(manifestXml));
+			
+			assertEquals("2013-05-28T16:50:43.999Z", manifest.getCreatedOn().toString());
+
+			// Can't test these - as Boris.omex manifest.xml only 
+			// list these as unconnected bnodes
 //			List<Agent> manifestCreator = manifest.getCreatedBy();			
 //			Agent createdBy = manifestCreator.get(0);
-//			assertEquals("Gary Mirams", createdBy.getName());
-//			assertEquals("mbox:gary.mirams@cs.ox.ac.uk", createdBy.getUri());
-//			assertEquals("2014-02-06T22:01:58Z", manifest.getCreatedOn().toString());
+//			assertEquals("Frank Bergmann", createdBy.getName());
+//			assertEquals("mbox:fbergman@caltech.edu", createdBy.getUri());
 //			
-//			
-//			
-//		}
+		}
 		
 	}
 	
@@ -98,6 +100,29 @@ public class TestCombineToRO {
 		}
 		CombineToRO.combineToRO(file);
 		System.out.println(file);		
+		
+		try (Bundle bundle = Bundles.openBundle(file)) {
+			Manifest manifest = manifest = bundle.getManifest();
+			Path manifestXml = bundle.getRoot().resolve("manifest.xml");
+			assertTrue("manifest.xml not listed in " + manifest.getManifest(), 
+					manifest.getManifest().contains(manifestXml));
+
+			Path boris = bundle.getRoot().resolve("BorisEJB.xml");
+			PathMetadata borisMeta = bundle.getManifest().getAggregation(boris);
+			assertEquals(URI.create("http://identifiers.org/combine.specifications/sbml"), borisMeta.getConformsTo());				
+			// dcterms:modified
+			assertEquals("2013-04-05T12:50:56Z", borisMeta.getCreatedOn().toString());
+
+			Path paperPdf = bundle.getRoot().resolve("paper").resolve("Kholodenko2000.pdf");
+			PathMetadata paperMeta = bundle.getManifest().getAggregation(paperPdf);
+			assertEquals("application/pdf", paperMeta.getMediatype());
+			
+			URI biomd = URI.create("http://www.ebi.ac.uk/biomodels-main/BIOMD0000000010");
+			PathMetadata biomdMeta = bundle.getManifest().getAggregation(biomd);
+			assertEquals(URI.create("http://identifiers.org/combine.specifications/sbml"), biomdMeta.getConformsTo());
+			
+		}
+		
 	}
 	
 	@Test
